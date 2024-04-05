c8ee1ba6b6150214b1c125aaa36e4fb12166dff3
ReyhanFaqihh <reyhanel1316@gmail.com>
2024-04-05 12:58:04 +0700

    Reyhan Faqih Ashuri

--- Bento-kopi/resources/views/partials/footer.blade.php
+++ Bento-kopi/resources/views/partials/footer.blade.php
@@ -19,12 +19,12 @@
     <div class="col mb-3 text-start">
         <h5 class="mt-3 mb-3 bold">Kontak</h5>
         <ul class="nav flex-column ">
-            <li class="nav-item mb-2 hoverU"><a href="#" class="nav-link p-0 text-body-secondary text-start">+62 812 1222 7772</a></li>
-            <li class="nav-item mb-2 hoverU"><a href="#" class="nav-link p-0 text-body-secondary text-start">bentokopi@gmail.com</a></li>
-            <li class="nav-item mb-2 hoverU"><a href="#" class="nav-link p-0 text-body-secondary text-start">Jl. Mengger, Sukapura, Kec. Dayeuhkolot, Kabupaten Bandung, Jawa Barat 40267</a></li>
+            <li class="nav-item mb-2 hoverU"><a href="https://wa.me/62895335021186" class="nav-link p-0 text-body-secondary text-start">+62 895-3350-21186</a></li>
+            <li class="nav-item mb-2 hoverU"><a href="mailto: bentokopi@gmail.com" class="nav-link p-0 text-body-secondary text-start">bentokopi@gmail.com</a></li>
+            <li class="nav-item mb-2 hoverU"><a href="https://maps.app.goo.gl/b9gyjg4zPNaAqTbr5" class="nav-link p-0 text-body-secondary text-start">Jl. Mengger, Sukapura, Kec. Dayeuhkolot, Kabupaten Bandung, Jawa Barat 40267</a></li>
         </ul>
-        <a class="logo" href="/"><img src="images/ig.png" alt=""></a>
-        <a class="logo" href="/"><img src="images/wa.png" alt=""></a>
-        <a class="logo" href="/"><img src="images/tiktok.png" alt=""></a>
+        <a class="logo" href="https://www.instagram.com/bentokopi.telkombandung"><img src="images/ig.png" alt=""></a>
+        <a class="logo" href="https://wa.me/62895335021186"><img src="images/wa.png" alt=""></a>
+        <a class="logo" href="https://www.tiktok.com/@bentokopiindonesia?is_from_webapp=1&sender_device=pc"><img src="images/tiktok.png" alt=""></a>
     </div>
 </footer>
