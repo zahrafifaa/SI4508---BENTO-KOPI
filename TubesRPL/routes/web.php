a8346743a9e46c2e705b42d2e641c9b96ad44fdb
fitrirhmdn <rahmafitri153@gmail.com>
2024-04-05 21:24:37 +0700

    BENTOKOPI-50

--- TubesRPL/routes/web.php
+++ TubesRPL/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/reservasi', function () {
+    return view('reservasi.blade.php');
+});
+
+Route::get('/ketersediaanmeja', function () {
+    return view('ketersediaanmeja.blade.php');
+});
