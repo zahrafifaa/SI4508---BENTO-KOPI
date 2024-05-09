fbde48da54547bba9de4c81f8b0499b4ba71af9b
zahrafifaa <zahraafifa2014@gmail.com>
2024-05-09 11:27:18 +0700

    BENTO-KOPI

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,9 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Database\Seeders\MenuSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,45 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        $this->call([
-            MenuSeeder::class,
-            DashboardKollabSeeder::class,
-        ]);
-
-        User::create([
-            'name' => 'user ',
-            'username' => 'User1 ',
-            'email' => 'user1@gmail.com',
-            'password' => bcrypt('123456')
-        ]);
-        
-        User::create([
-            'name' => 'adminCashier ',
-            'username' => 'Cashier ',
-            'email' => 'adminCashier@gmail.com',
-            'password' => bcrypt('123456')
-        ]);
-        
-        User::create([
-            'name' => 'Super Admin ',
-            'username' => 'SuperAdmin',
-            'email' => 'adminApp@gmail.com',
-            'password' => bcrypt('123456')
-        ]);
-
-        User::create([
-            'name' => 'faqih ',
-            'username' => 'faqih',
-            'email' => 'faqih@mail.com',
-            'password' => bcrypt('123456')
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
         ]);
     }
-
-    
 }
--- resources/views/login/mail-reset-password.blade.php
+++
@@ -1,4 +0,0 @@
-<h4>Halo, Anda diminta untuk mereset password, gunakan link dibawah ini untuk mereset password Anda</h4>
-<br>
-<br>
-<a href="{{ route('validate-forgot-password',['token' => $token]) }}">Klik Disini</a>
