15be9afbc827e90dbed92948f8549411093407a3
fitrirhmdn <rahmafitri153@gmail.com>
2024-05-27 20:45:46 +0700

    Cleared all files from the branch

--- resources/views/kolaborasi.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layout/main')
-
-
-@section('isiPage')
-    <h1>Halaman Kolaborasi</h1>
-@endsection
--- resources/views/pages/reservasi/editreservasi.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layout/main')
-
-
-@section('isiPage')
-<h1>Edit Reservasi</h1><br>
-@endsection
