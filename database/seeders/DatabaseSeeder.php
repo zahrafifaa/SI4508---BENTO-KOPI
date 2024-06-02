2dd48ade58b6b283b262340ded6f13225d321906
zahrafifaa <zahraafifa2014@gmail.com>
2024-06-02 20:06:07 +0700

    Zahra

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Cart;
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
