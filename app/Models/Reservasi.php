9efcab5f2d0e11f275f5083d3b0f3919ad7ac096
fitrirhmdn <rahmafitri153@gmail.com>
2024-06-03 20:36:17 +0700

    BENKOP-8

--- app/Models/Reservasi.php
+++ app/Models/Reservasi.php
@@ -18,15 +18,23 @@ class Reservasi extends Model
 
     public static function getNewCode()
     {
-        $lastCode = static::orderBy('created_at', 'desc')->first();
-        if (!$lastCode) {
-            $newCodeNumber = 1;
-        } else {
-            $lastCodeNumber = (int) substr($lastCode->kode, 1);
-            $newCodeNumber = $lastCodeNumber + 1;
-        }
-        $newCode = 'RES' . str_pad($newCodeNumber, 5, '0', STR_PAD_LEFT);
-        return $newCode;
+    // Mendapatkan entri terbaru berdasarkan tanggal pembuatan
+    $last = static::latest('created_at')->first();
+
+    // Jika tidak ada entri sebelumnya, mulai dari 1
+    if (!$last || !isset($last->kode)) {
+        $newCodeNumber = 1;
+    } else {
+        // Mengambil bagian angka dari kode terakhir dan menambahnya dengan 1
+        $lastCode = $last->kode;
+        $lastCodeNumber = (int) substr($lastCode, 3); // Mengubah 1 menjadi 3 untuk mengambil bagian angka setelah 'RES'
+        $newCodeNumber = $lastCodeNumber + 1;
+    }
+
+    // Membuat kode baru dengan format 'RES' diikuti angka 5 digit
+    $newCode = 'RES' . str_pad($newCodeNumber, 5, '0', STR_PAD_LEFT);
+
+    return $newCode;
     }
 
     public function status()
